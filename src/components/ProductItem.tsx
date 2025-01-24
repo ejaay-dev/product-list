b8e84ccd01e74460fdb663b404de9ae24d7d14ad
ejaay-dev <ejaay.dev@gmail.com>
2025-01-25 00:57:18 +0800

    Fifth commit Added functionality for add to cart, add product quantity, and minus product quantity

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 interface ProductItemProps {
   productName: string
   productDescription: string
@@ -9,6 +11,26 @@ export const ProductItem = ({
   productDescription,
   productPrice,
 }: ProductItemProps) => {
+  const [isSelectedDessert, setIsSelectedDesssert] = useState<boolean>(false)
+  const [selectedProductQuantity, setSelectedProductQuantity] =
+    useState<number>(0)
+
+  const handleAddToCart = () => {
+    setIsSelectedDesssert(true)
+    setSelectedProductQuantity(1)
+  }
+
+  const handleIncrement = () => {
+    setSelectedProductQuantity(selectedProductQuantity + 1)
+  }
+
+  const handleDecrement = () => {
+    setSelectedProductQuantity(selectedProductQuantity - 1)
+    if (selectedProductQuantity === 1) {
+      setIsSelectedDesssert(false)
+    }
+  }
+
   return (
     <>
       <div className="flex flex-col justify-center font-redhat">
@@ -16,14 +38,61 @@ export const ProductItem = ({
           <img
             src="/public/assets/images/image-creme-brulee-mobile.jpg"
             alt="Crème Brûlée"
-            className="rounded-xl"
+            className={`rounded-xl ${
+              isSelectedDessert ? "border-2 border-custom-red" : ""
+            }`}
           />
-          <div className="absolute bottom-[-20px] flex flex-row justify-center items-center gap-2 border border-custom-rose-500 w-40 h-10 bg-white rounded-3xl">
-            <img
-              src="/public/assets/images/icon-add-to-cart.svg"
-              alt="Add to Cart"
-            />
-            <p className="font-medium text-custom-rose-900">Add to Cart</p>
+          <div className="absolute bottom-[-20px]">
+            {isSelectedDessert ? (
+              <div className="flex flex-row justify-around items-center gap-2 border border-custom-rose-500 w-40 h-10 bg-custom-red rounded-3xl">
+                <div onClick={handleDecrement}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    width="16"
+                    height="16"
+                    className="h-5 w-5 border border-white rounded-full cursor-pointer text-white hover:text-custom-red hover:bg-white"
+                  >
+                    {/* Horizontal Line */}
+                    <path d="M4.5 9h9" />
+                  </svg>
+                </div>
+                <p className="font-normal text-white">
+                  {selectedProductQuantity}
+                </p>
+                <div onClick={handleIncrement}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    width="16"
+                    height="16"
+                    className="h-5 w-5 border border-white rounded-full cursor-pointer text-white hover:text-custom-red hover:bg-white"
+                  >
+                    {/* Vertical Line */}
+                    <path d="M9 4v10" />
+                    {/* Horizontal Line */}
+                    <path d="M4 8.5h10" />
+                  </svg>
+                </div>
+              </div>
+            ) : (
+              <div
+                onClick={handleAddToCart}
+                className="flex flex-row justify-center items-center gap-2 border border-custom-rose-500 hover:border-custom-red w-40 h-10 bg-white rounded-3xl"
+              >
+                <img
+                  src="/public/assets/images/icon-add-to-cart.svg"
+                  alt="Add to Cart"
+                />
+                <p className="font-medium text-custom-rose-900 hover:text-custom-red">
+                  Add to Cart
+                </p>
+              </div>
+            )}
           </div>
         </div>
 
